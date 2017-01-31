f5b7ae20d4350ca0e01d2b2dd539632df7a083b8
game110pm <game110pm@605-110-STU08>
2017-01-31 12:47:28 -0700

    1/31/17

---
+++ Bad joke/Bad joke/Source.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "I have a music playlist when I go hiking. It consists of Earth Wind and Fire, Bruno Mars, Green Day, and Eminem." << endl << endl << endl << endl << endl << endl << endl;
+	cout << "I call it my trail mix." << endl;
+}
